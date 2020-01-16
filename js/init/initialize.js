72eeba8fab81e40fbd62714dd5d8a6e254affb9e
adisaran64 <59209660+adisaran64@users.noreply.github.com>
2020-01-15 17:20:18 -0800

    Massive simplification of nav.

--- js/init/initialize.js
+++ js/init/initialize.js
@@ -7,14 +7,6 @@
 var tl = gsap.timeline();
 
 
-// Create Sub-Timelines for Nav Hover Animations
-
-var hometl = gsap.timeline({paused: true, repeat: -1});
-var resumetl = gsap.timeline({paused: true, repeat: -1});
-var portfoliotl = gsap.timeline({paused: true, repeat: -1});
-var linkstl = gsap.timeline({paused: true, repeat: -1});
-
-
 // Prevent Flickering on Load with Pre-Loading Before Start of Timeline
 
 document.addEventListener("DOMContentLoaded", function(event) {
@@ -22,9 +14,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
         document.body.style.display = "block";
         homePageAnimation();
         descriptionTypeWriter();
-        createHomeAnimation();
-        createResumeAnimation();
-        createPortfolioAnimation();
-        createLinksAnimation();
     }, false);
 });
